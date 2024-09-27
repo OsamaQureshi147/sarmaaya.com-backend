e6fcb86a05a47b9a8dc97221426f90588a7c7d41
OsamaImadi <osamaimadi@yahoo.com>
2024-09-27 11:36:36 +0500

    Added ownership endpoints

--- src/modules/asset-details/asset-details.module.ts
+++ src/modules/asset-details/asset-details.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AssetDetailsEntity } from 'lib-typeorm-pro';
-
-<<<<<<< HEAD
 import { AssetDetailsService } from './asset-details.service';
-=======
->>>>>>> 757fdfcbe8661ef5af3e86d00f8172bda3ea9991
 import { AssetDetailsController } from './asset-details.controller';
 
 @Module({
