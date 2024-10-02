1e55c7a1fe986be8da664552c78ce7a42ca6884f
OsamaImadi <osamaimadi@yahoo.com>
2024-10-02 13:09:39 +0500

    Fixed response for funds ownership

--- src/modules/asset-ownerships/asset-ownerships.service.ts
+++ src/modules/asset-ownerships/asset-ownerships.service.ts
@@ -2,7 +2,7 @@ import { Injectable, OnModuleInit, NotFoundException } from '@nestjs/common';
 import { ApiClient, FundHoldingsApi, SecurityHoldersApi } from '@factset/sdk-factsetownership';
 import { AssetOwnershipEntity, AssetOwnershipDto } from 'lib-typeorm-pro';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository} from 'typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AssetOwnershipsService implements OnModuleInit {
@@ -11,7 +11,7 @@ export class AssetOwnershipsService implements OnModuleInit {
 
   constructor(
     @InjectRepository(AssetOwnershipEntity)
-    private readonly assetOwnershipRepository: Repository<AssetOwnershipEntity>,) {}
+    private readonly assetOwnershipRepository: Repository<AssetOwnershipEntity>,) { }
 
   async onModuleInit() {
     this.initializeApi();
@@ -34,47 +34,65 @@ export class AssetOwnershipsService implements OnModuleInit {
     this.fundHoldingsApi = new FundHoldingsApi(apiClient);
     this.securityHoldersApi = new SecurityHoldersApi(apiClient);
   }
-  
+
   async create(assetOwnershipDto: AssetOwnershipDto): Promise<AssetOwnershipEntity> {
     const assetDetails = this.assetOwnershipRepository.create(assetOwnershipDto);
     return this.assetOwnershipRepository.save(assetDetails);
   }
 
-async findSecurityHolders(query: any): Promise<AssetOwnershipEntity[]> {
+  async findSecurityHolders(query: any): Promise<AssetOwnershipEntity[]> {
 
     const { securityId, date, holderType, topn, currency } = query;
-  
+
     const ids = Array.isArray(securityId) ? securityId : [securityId];
-  
+
     const data = await this.securityHoldersApi.getSecurityHolders(ids, {
       date,
       holderType,
       topn,
       currency,
     });
-  
-    return data; 
+
+    return data;
   }
-  
-  
+
+
   async removeSecurityHolder(securityId: string, date: string): Promise<{ message: string }> {
     throw new NotFoundException(`Security holder with ID ${securityId} cannot be deleted.`);
   }
 
-  async getEquityHolders(isin: string): Promise<{ companyName: string; percentOutstanding: number; adjMarketValue: number }[]> {
+  async getEquityHolders(isin: string) {
     const results = await this.assetOwnershipRepository.createQueryBuilder('ownership')
-   .select([
-            'ownership.holderName AS companyName',         
-            'ownership.date AS date',                       
-            'ownership.adjMarketValue AS adjMarketValue',   
-            'ownership.percentageOutstanding AS percentOutstanding' 
-        ])
-        .where('ownership.holderType = :holderType', { holderType: 'Institution' }) 
-        .andWhere('ownership.isin = :isin', { isin }) 
-        .getRawMany(); 
+      .select([
+        'ownership.holderName AS companyName',
+        'ownership.date AS date',
+        'ownership.adjMarketValue AS adjMarketValue',
+        'ownership.percentageOutstanding AS percentOutstanding',
+        'ownership.adjHolding AS adjustedHolding',
+      ])
+      .where('ownership.holderType = :holderType', { holderType: 'Institution' })
+      .andWhere('ownership.isin = :isin', { isin })
+      .getRawMany();
 
-    return results;
-}
+    let response = {}
+
+    results.forEach(item => {
+      const { companyname, date, adjmarketvalue, percentoutstanding, adjustedholding } = item;
+
+      if (!response[companyname]) {
+        response[companyname] = [];
+      }
+
+      response[companyname].push({
+        date,
+        adjmarketvalue,
+        percentoutstanding,
+        adjustedholding
+      });
+    });
+
+    return response;
+  }
 
 }
 
